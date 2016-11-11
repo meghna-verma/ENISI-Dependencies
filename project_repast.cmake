66cf79786f35cdfec136a65bc2f65fc0c7ba934d
Stefan Hoops <shoops@vbi.vt.edu>
2016-11-11 16:22:03 -0500

    Fixed to build MPI if none is externalyy provided.

--- project_repast.cmake
+++ project_repast.cmake
@@ -31,7 +31,8 @@ if("${BUILD_STEP}" STREQUAL "patch")
 endif()
 
 if("${BUILD_STEP}" STREQUAL "install")
-  if (NOT EXISTS .mpich_install_done AND ${MPI_CXX_COMPILER} STREQUAL "" )
+  if (NOT EXISTS .mpich_install_done AND 
+      (NOT ${MPI_CXX_COMPILER} OR ${MPI_CXX_COMPILER} STREQUAL "" ))
     message(STATUS "Installing MPICH...")
     file(REMOVE_RECURSE ${REPAST_EXT_DIR}/MPICH)
     execute_process(
