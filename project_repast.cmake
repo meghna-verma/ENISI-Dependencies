a3509427383c34f02fa1330d0d18874ba9d15ae0
Stefan Hoops <shoops@vbi.vt.edu>
2016-11-11 19:32:40 -0500

    Improved installation of rapast dependencies.

--- project_repast.cmake
+++ project_repast.cmake
@@ -36,7 +36,7 @@ if("${BUILD_STEP}" STREQUAL "install")
     message(STATUS "Installing MPICH...")
     file(REMOVE_RECURSE ${REPAST_EXT_DIR}/MPICH)
     execute_process(
-      COMMAND bash ./install.sh mpich
+      COMMAND bash ./install.sh mpich "${INSTALL_PREFIX}"
       WORKING_DIRECTORY ${WORK_DIR}
       OUTPUT_FILE ${LOG_DIR}/mpich_install.log
       ERROR_FILE ${LOG_DIR}/mpich_install_errors.log
@@ -47,7 +47,7 @@ if("${BUILD_STEP}" STREQUAL "install")
   if (NOT EXISTS .boost_install_done)
     message(STATUS "Installing Boost...")
     file(REMOVE_RECURSE ${REPAST_EXT_DIR}/Boost)
-    execute_process(COMMAND bash ./install.sh boost ${MPI_CXX_COMPILER}
+    execute_process(COMMAND bash ./install.sh boost "${INSTALL_PREFIX}" ${MPI_CXX_COMPILER}
       WORKING_DIRECTORY ${WORK_DIR}
       OUTPUT_FILE ${LOG_DIR}/boost_install.log
       ERROR_FILE ${LOG_DIR}/boost_install_errors.log
@@ -58,7 +58,7 @@ if("${BUILD_STEP}" STREQUAL "install")
   if (NOT EXISTS .netcdf_install_done)
     message(STATUS "Installing NetCDF...")
     file(REMOVE_RECURSE ${REPAST_EXT_DIR}/NetCDF)
-    execute_process(COMMAND bash ./install.sh netcdf-netcpp 
+    execute_process(COMMAND bash ./install.sh netcdf-netcpp "${INSTALL_PREFIX}"
       WORKING_DIRECTORY ${WORK_DIR}
       OUTPUT_FILE ${LOG_DIR}/netcdf_install.log
       ERROR_FILE ${LOG_DIR}/netcdf_install_errors.log
@@ -69,7 +69,7 @@ if("${BUILD_STEP}" STREQUAL "install")
   if (NOT EXISTS .curl_install_done)
     message(STATUS "Installing Curl...")
     file(REMOVE_RECURSE ${REPAST_EXT_DIR}/CURL)
-    execute_process(COMMAND bash ./install.sh lcurl
+    execute_process(COMMAND bash ./install.sh lcurl "${INSTALL_PREFIX}"
       WORKING_DIRECTORY ${WORK_DIR}
       OUTPUT_FILE ${LOG_DIR}/lcurl_install.log
       ERROR_FILE ${LOG_DIR}/lcurl_install_errors.log
@@ -79,7 +79,7 @@ if("${BUILD_STEP}" STREQUAL "install")
 
   if (NOT EXISTS ${REPAST_DIR}/lib/librepast_hpc-2.0.a)
     message(STATUS "Installing Repast HPC libraries...")
-    execute_process(COMMAND bash ./install.sh rhpc "${MPI_CXX_COMPILER}" "${MPI_CXX_LINK_FLAGS}"
+    execute_process(COMMAND bash ./install.sh rhpc "${INSTALL_PREFIX}" "${MPI_CXX_COMPILER}" "${MPI_CXX_LINK_FLAGS}"
       WORKING_DIRECTORY ${WORK_DIR}
       OUTPUT_FILE ${LOG_DIR}/rhpc_install.log
       ERROR_FILE ${LOG_DIR}/rhpc_install_errors.log
